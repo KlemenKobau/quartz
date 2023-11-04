bc2171c852862d00c67c1c948a3a743b0c85a722
Klemen Kobau <klemen.kobau@gmail.com>
2023-11-04 19:34:16 +0100

    feat: renamed

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Klemen's second brain",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
