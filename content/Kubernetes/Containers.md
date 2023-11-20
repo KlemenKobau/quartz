cfc41e24899a80e748e699fcd7c3a22c507dabde
Klemen Kobau <klemen.kobau@gmail.com>
2023-11-20 18:40:36 +0100

    feat: updated namespaces

--- content/Kubernetes/Containers.md
+++ content/Kubernetes/Containers.md
@@ -3,3 +3,5 @@
 - [Quadlet](https://www.redhat.com/sysadmin/quadlet-podman)
 
 ## Docker
+
+- [Docker compose](https://docs.docker.com/compose/)
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -29,7 +29,12 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
+    Component.Graph({
+      localGraph: {
+        depth: 2
+      },
+      globalGraph: {}
+    }),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
